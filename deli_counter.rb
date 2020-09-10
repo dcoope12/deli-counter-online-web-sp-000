8393a1fd461b425d164efbb24552570341efc0f0
Devin Cooper <dcoope12@asu.edu>
2020-09-10 01:45:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  line_positions = [];
+if katz_deli.empty?
+puts "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+puts "#{name}, #{katz_deli + 1}"
+end
